f0e8840319f4917254f09b0d14243b7bd820ee6d
parom <E.Parominskiy@isida.by>
2023-10-06 14:29:31 +0300

    First Query with GraphQL

---
+++ First-GraphQL-With-Spring-Boot-App/src/main/java/com/example/demo/query/Query.java
@@ -0,0 +1,12 @@
+package com.example.demo.query;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Query implements GraphQLQueryResolver {
+
+    public String firstQuery(){
+        return "First Query";
+    }
+}
