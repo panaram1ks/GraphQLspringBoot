cbeb1e9fbf9eb4ae538892bd62ebf69f74639d77
parom <E.Parominskiy@isida.by>
2023-10-06 09:23:43 +0300

    Lombok, getters, setters, @Data

--- Spring-Boot-App-For-Lombok/src/main/java/com/example/response/StudentResponse.java
+++ Spring-Boot-App-For-Lombok/src/main/java/com/example/response/StudentResponse.java
@@ -2,7 +2,14 @@ package com.example.response;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
 
+
+@Getter
+@Setter
+//@ToString
+//@EqualsAndHashCode
+//@Data // is equal  @Getter @Setter @ToString @EqualsAndHashCode
 public class StudentResponse {
 
 	@JsonIgnore
@@ -16,30 +23,6 @@ public class StudentResponse {
 	public StudentResponse(long id, String firstName, String lastName) {
 		this.id = id;
 		this.firstName = firstName;
-		this.lastName = lastName;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
