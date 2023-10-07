69ceee3228652d81196cfac58b0f0b3a2c43fe02
evgen <parom.evgen@mail.ru>
2023-10-07 16:08:42 +0300

    Run Query with GraphQL and Spring Boot

--- First-GraphQL-With-Spring-Boot-App/src/main/java/com/example/demo/query/Query.java
+++ First-GraphQL-With-Spring-Boot-App/src/main/java/com/example/demo/query/Query.java
@@ -9,4 +9,8 @@ public class Query implements GraphQLQueryResolver {
     public String firstQuery(){
         return "First Query";
     }
+
+    public String secondQuery(){
+        return "Second Query";
+    }
 }
