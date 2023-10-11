83f2fa3581d9008f5ef185af04256c96f4e88b70
parom <E.Parominskiy@isida.by>
2023-10-11 12:32:10 +0300

    25. Flexibility Of Response with GraphQL Query

--- First-GraphQL-With-Spring-Boot-App/src/main/java/com/example/demo/repository/SubjectRepository.java
+++ First-GraphQL-With-Spring-Boot-App/src/main/java/com/example/demo/repository/SubjectRepository.java
@@ -3,7 +3,7 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.entity.Subject;
+import com.example.demo.entity.Subject;
 
 @Repository
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
