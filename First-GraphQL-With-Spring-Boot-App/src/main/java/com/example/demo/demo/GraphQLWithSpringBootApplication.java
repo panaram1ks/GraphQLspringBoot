8d201b13fe44d3857109ea3e8f28dcf734bef66e
parom <E.Parominskiy@isida.by>
2023-10-17 09:56:43 +0300

    38. Running First Mutation with GraphQL

--- First-GraphQL-With-Spring-Boot-App/src/main/java/com/example/demo/demo/GraphQLWithSpringBootApplication.java
+++ First-GraphQL-With-Spring-Boot-App/src/main/java/com/example/demo/demo/GraphQLWithSpringBootApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan({"com.example.demo.query", "com.example.demo.service", "com.example.demo.resolver"})
+@ComponentScan({"com.example.demo.query", "com.example.demo.service", "com.example.demo.resolver", "com.example.demo.mutation"})
 @EntityScan("com.example.demo.entity")
 @EnableJpaRepositories("com.example.demo.repository")
 public class GraphQLWithSpringBootApplication {
