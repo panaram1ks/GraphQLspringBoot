e7fa1c97aaa946a5bd97062000e016ec4724b529
parom <E.Parominskiy@isida.by>
2023-10-17 08:49:37 +0300

    34. Get Filtered Data with GraphQL Edge

---
+++ First-GraphQL-With-Spring-Boot-App/src/main/java/com/example/demo/enums/SubjectNameFilter.java
@@ -0,0 +1,10 @@
+package com.example.demo.enums;
+
+/**
+ * @author E.Parominsky 17/10/2023 08:32
+ */
+public enum SubjectNameFilter {
+    Java,
+    MySQL,
+    MongoDB
+}
--- First-GraphQL-With-Spring-Boot-App/src/main/java/com/example/demo/resolver/StudentResponseResolver.java
+++ First-GraphQL-With-Spring-Boot-App/src/main/java/com/example/demo/resolver/StudentResponseResolver.java
@@ -3,6 +3,7 @@ package com.example.demo.resolver;
 import com.coxautodev.graphql.tools.GraphQLResolver;
 import com.example.demo.entity.Student;
 import com.example.demo.entity.Subject;
+import com.example.demo.enums.SubjectNameFilter;
 import com.example.demo.response.StudentResponse;
 import com.example.demo.response.SubjectResponse;
 import org.springframework.stereotype.Service;
@@ -15,12 +16,14 @@ public class StudentResponseResolver implements GraphQLResolver<StudentResponse>
 
     // method should be public, we should not invoke it manualy!!
 
-    public List<SubjectResponse> getLearningSubjects(StudentResponse studentResponse) {
-        ArrayList<SubjectResponse> learningSubjects = new ArrayList<SubjectResponse>();
+    public List<SubjectResponse> getLearningSubjects(StudentResponse studentResponse, SubjectNameFilter subjectNameFilter) {
+        List<SubjectResponse> learningSubjects = new ArrayList<SubjectResponse>();
         Student student = studentResponse.getStudent();
         if (student.getLearningSubjects() != null) {
             for (Subject subject : student.getLearningSubjects()) {
-                learningSubjects.add(new SubjectResponse(subject));
+               if(subjectNameFilter.name().equalsIgnoreCase(subject.getSubjectName())){
+                   learningSubjects.add(new SubjectResponse(subject));
+               }
             }
         }
         return learningSubjects;
