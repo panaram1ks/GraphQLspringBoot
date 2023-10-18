ce39dc30da2d14e75e96c5507b92d9bc9606a404
parom <E.Parominskiy@isida.by>
2023-10-18 12:21:20 +0300

    60. Create GraphQL Client Spring Boot App

---
+++ Create-GraphQL-Client-Spring-Boot-App/src/main/java/com/infybuzz/service/ClientService.java
@@ -0,0 +1,13 @@
+package com.infybuzz.service;
+
+import org.springframework.stereotype.Service;
+
+import com.infybuzz.response.StudentResponse;
+
+@Service
+public class ClientService {
+
+	public StudentResponse getStudent (Integer id) {
+		
+	}
+}
